0aee7176a8a5ec619a0a051f994b76ecc1b1e0d7
rajasaran256 <38044070+rajasaran256@users.noreply.github.com>
2019-12-31 03:03:57 +0000

    Infrastructure code

---
+++ modules/gcs/variables.tf
@@ -0,0 +1,12 @@
+
+variable "project" {
+  description = "The ID of the project where subnets will be created"
+}
+
+variable "name" {
+  description = "The name of the GCS bucket being created"
+}
+
+variable "storage_class" {
+  description = "The storage class of GCS bucket being created"
+}
